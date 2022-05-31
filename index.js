ade686c02cde4b34b454a8e0cb72462c08478907
Rafael Carpio <oleafarx@gmail.com>
2022-05-31 15:47:08 -0600

    Se crea el mapa visualemente y el mapa en js

---
+++ index.js
@@ -0,0 +1,23 @@
+//Se crea el mapa con arrays. 
+const mapa = Array(4);
+mapa[0] = Array(4);
+mapa[1] = Array(4);
+mapa[2] = Array(4);
+mapa[3] = Array(4);
+mapa[0][0] = ["", "", ""];
+mapa[0][1] = ["","Hedor",""];
+mapa[0][2] = ["Wumpus","",""];
+mapa[0][3] = ["","Hedor",""];
+mapa[1][0] = ["","","Brisa"];
+mapa[1][1] = ["","",""];
+mapa[1][2] = ["Oro", "Hedor","Brisa"];
+mapa[1][3] = ["","",""];
+mapa[2][0] = ["Hoyo","",""];
+mapa[2][1] = ["","","Brisa"];
+mapa[2][2] = ["Hoyo", "", ""];
+mapa[2][3] = ["", "", "Brisa"];
+mapa[3][0] = ["","","Brisa"];
+mapa[3][1] = ["","",""];
+mapa[3][2] = ["","","Brisa"];
+mapa[3][3] = ["Hoyo","",""];
+
